87c1d991dc5ac149cdae5addbdd5af7f08a27f89
Antonov Denis <den_ru1982@mail.ru>
2022-08-17 16:54:13 +0300

    Добавил комментария

--- generatorPseudoRandomNumbers/generator.go
+++ generatorPseudoRandomNumbers/generator.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+//Получаем число, до которого надо взять случайное
+//число и возваращаем псевдо-случайное число
 func Generating(n int) int {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return r.Intn(n)
