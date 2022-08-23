ab05c8f54bd99b87318c8e9636b91c561a4153ae
Antonov Denis <den_ru1982@mail.ru>
2022-08-23 16:25:17 +0300

    Проверка на дубликаты работает только с отсортированным слайсом

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	slice := []int{1,2,0,1,3,2,2,3,4,5,5}
+	slice := []int{1,2,3,3,4,5,5}
 	result := structures.CreateListNode(slice)
 	mapList := make(map[int]*structures.ListNode)
 	structures.DeleteDublicates(&result,mapList)
