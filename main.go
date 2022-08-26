81da409b95959812ef894bf5292c3490ff4ec4fe
Antonov Denis <den_ru1982@mail.ru>
2022-08-26 13:38:29 +0300

    Сделана задача по объединению двух слейсов в один где в одном четные в другом нечетные элементы.

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"github.com/adminsemy/golangTests/structures"
-)
+import jointwoslice "github.com/adminsemy/golangTests/joinTwoSlice"
 
 func main() {
-	slice := []int{1,2,3,3,4,5,5}
-	result := structures.CreateListNode(slice)
-	mapList := make(map[int]*structures.ListNode)
-	structures.DeleteDublicates(&result,mapList)
-	structures.DoubleList(result)
-
+	jointwoslice.Join()
 }
