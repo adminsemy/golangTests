cb5819e48ccb0f78313ec19f52ffb40c282a99e6
Antonov Denis <den_ru1982@mail.ru>
2022-11-02 19:27:33 +0300

    Продолжаем реализовывать алгоритм

--- kmp/kmp.go
+++ kmp/kmp.go
@@ -14,18 +14,23 @@ func CreatePi(str string) {
 
 	strRune := []rune(str)
 
+	pi = make([]int, len(strRune))
+
 	for i, symbol := range strRune {
+		pi[i] = 0
 		fmt.Println(i, symbol)
 		if i == 0 {
 			prefics = symbol
 			continue
 		}
 		suffics = symbol
+
 		if suffics != prefics {
 			if iPrefics == 0 {
 				pi[i] = 0
 			} else {
 				iPrefics = pi[iPrefics-1]
+				prefics = strRune[pi[iPrefics]]
 			}
 		}
 
@@ -35,4 +40,5 @@ func CreatePi(str string) {
 		}
 		prefics = strRune[pi[iPrefics]]
 	}
+	fmt.Println(pi)
 }
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	kmp.CreatePi("Привет")
+	kmp.CreatePi("abba")
 }
