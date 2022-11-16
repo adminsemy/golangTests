232b9307f6965f074503561e8ccdbf8784ed2c1b
Antonov Denis <den_ru1982@mail.ru>
2022-11-16 14:56:09 +0300

    Доделан алгоритм создания слайса данных по подстроке

--- kmp/kmp.go
+++ kmp/kmp.go
@@ -4,29 +4,33 @@
 
 package kmp
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var pi []int
 
 func CreatePi(str string) {
 	var i, iPrefics int
-
 	strRune := []rune(str)
-
 	pi = make([]int, len(strRune))
-
 	i = 1
-	for range strRune {
-		if strRune[i] == strRune[iPrefics] {
+	for {
+		if i == len(strRune) {
+			break
+		}
+		switch {
+		case strRune[i] == strRune[iPrefics]:
 			pi[i] = iPrefics + 1
 			iPrefics++
 			i++
-		} else if iPrefics == 0 {
+		case iPrefics == 0:
 			pi[i] = 0
 			i++
-		} else {
+		default:
 			iPrefics = pi[iPrefics-1]
 		}
+
 	}
 	fmt.Println(pi)
 }
