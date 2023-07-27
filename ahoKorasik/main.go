5b117bb3f5787259f6ef9d3b7eb6b3c2c204275e
Antonov Denis <den_ru1982@mail.ru>
2023-07-27 13:07:57 +0300

    Доделан алгоритм работы Ахо-Корасика

--- ahoKorasik/main.go
+++ ahoKorasik/main.go
@@ -8,28 +8,38 @@ import (
 var ErrorNotFoundTree = errors.New("Tree is not found on parent")
 
 type Bor struct {
-	id       rune
-	children []*Bor
-	parent   *Bor
-	suffics  *Bor
-	final    bool
+	id        rune
+	fullName  []rune
+	children  []*Bor
+	parent    *Bor
+	suffix    *Bor
+	finalLink *Bor
+	final     bool
 }
 
+// Печатает бор деревьев для визуальной наглядности.
+// Существенной пользы не приносит
 func printBor(bor *Bor, prefix string) {
-	fmt.Println(prefix, string(bor.id))
+	fmt.Println(prefix, "id", string(bor.id))
+	fmt.Println(prefix, "fullName", string(bor.fullName))
 	if bor.parent != nil {
 		if bor.parent.id == 0 {
-			fmt.Println(prefix, "<root>")
+			fmt.Println(prefix, "parent", "<root>")
 		} else {
-			fmt.Println(prefix, string(bor.parent.id))
+			fmt.Println(prefix, "parent", string(bor.parent.id))
 		}
 	} else {
-		fmt.Println(prefix, "<root>")
+		fmt.Println(prefix, "parent", "<root>")
 	}
-	if bor.suffics != nil {
-		fmt.Println(prefix, *bor.suffics)
+	if bor.suffix != nil {
+		fmt.Println(prefix, "suffix", string(bor.suffix.id))
 	}
-	fmt.Println(prefix, bor.final)
+	fmt.Println(prefix, "final", bor.final)
+	if bor.finalLink != nil {
+		fmt.Println(prefix, "finalLink", string(bor.finalLink.fullName))
+	} else {
+		fmt.Println(prefix, "finalLink", "<root>")
+	}
 	if len(bor.children) > 0 {
 		for _, c := range bor.children {
 			printBor(c, prefix+prefix)
@@ -52,6 +62,9 @@ func main() {
 	}
 	bor := createBor(str)
 	printBor(bor, "-")
+	for _, str := range findAll(*bor, "CARABASS BARABAS") {
+		fmt.Println(string(str))
+	}
 }
 
 func findBor(s rune, tree *Bor) (*Bor, error) {
@@ -73,11 +86,99 @@ func createBor(findStr []string) *Bor {
 			last, err = findBor(s, tree)
 			if err != nil {
 				last = &Bor{id: s, parent: tree}
+				fullName := tree.fullName
+				fullName = append(fullName, last.id)
+				last.fullName = fullName
 				tree.children = append(tree.children, last)
 			}
 			tree = last
 		}
 		last.final = true
 	}
+	var suffixBor []*Bor
+	for _, b := range bor.children {
+		b.suffix = &bor
+		suffixBor = append(suffixBor, b.children...)
+	}
+	createSuffix(suffixBor, &bor)
+	createFinalLink(suffixBor)
 	return &bor
+}
+
+func createSuffix(bor []*Bor, mainBor *Bor) {
+	var newBor []*Bor
+	for _, b := range bor {
+		childParentSuffix := b.parent.suffix.children
+		for _, cps := range childParentSuffix {
+			if b.id == cps.id {
+				b.suffix = cps
+				break
+			}
+			b.suffix = mainBor
+		}
+		newBor = append(newBor, b.children...)
+	}
+	if len(newBor) == 0 {
+		return
+	}
+	createSuffix(newBor, mainBor)
+}
+
+func createFinalLink(bor []*Bor) {
+	var newBor []*Bor
+	for _, b := range bor {
+		b.finalLink = findFinalLink(b.suffix)
+		newBor = append(newBor, b.children...)
+	}
+	if len(newBor) == 0 {
+		return
+	}
+	createFinalLink(newBor)
+}
+
+func findFinalLink(tree *Bor) *Bor {
+	if tree.final {
+		return tree
+	}
+	if tree.suffix == nil {
+		return nil
+	}
+	return findFinalLink(tree.suffix)
+}
+
+func findAll(bor Bor, text string) [][]rune {
+	var res [][]rune
+	var tree *Bor
+	tree = &bor
+	for _, t := range text {
+		tree = findCurrentTree(tree, t)
+		if tree.final {
+			res = append(res, tree.fullName)
+		}
+		if tree.finalLink != nil {
+			res = append(res, resFinalLink(tree.finalLink)...)
+		}
+	}
+	return res
+}
+
+func findCurrentTree(bor *Bor, t rune) *Bor {
+	for _, c := range bor.children {
+		if c.id == t {
+			return c
+		}
+	}
+	if bor.suffix == nil {
+		return bor
+	}
+	return findCurrentTree(bor.suffix, t)
+}
+
+func resFinalLink(tree *Bor) [][]rune {
+	var res [][]rune
+	res = append(res, tree.fullName)
+	if tree.finalLink != nil {
+		res = append(res, resFinalLink(tree.finalLink)...)
+	}
+	return res
 }
